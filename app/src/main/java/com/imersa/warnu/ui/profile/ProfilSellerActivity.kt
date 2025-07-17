1739277e06c2722a587c6d27654fb8167864bc65
ugw20 <unggulgalih20@gmail.com>
2025-07-17 17:38:07 +0700

    tampilan seller done semua -Nugroho

---
+++ app/src/main/java/com/imersa/warnu/ui/profile/ProfilSellerActivity.kt
@@ -0,0 +1,30 @@
+package com.imersa.warnu.ui.profile
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.imersa.warnu.R
+
+class ProfilSellerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profil_seller)
+
+        // Mengisi data langsung ke setiap TextView dengan ID unik
+        findViewById<TextView>(R.id.tvNamaValue).text = "Aldi Seller"
+        findViewById<TextView>(R.id.tvEmailValue).text = "aldiseller@gmail.com"
+        findViewById<TextView>(R.id.tvTeleponValue).text = "XXXXXXXXXX"
+        findViewById<TextView>(R.id.tvAlamatValue).text = "XXXXXXXXXX"
+        findViewById<TextView>(R.id.tvRoleValue).text = "Aldixxxxxx"
+
+
+        // Kode bawaan untuk mengatur padding system bar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
